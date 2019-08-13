a214029c31b2b73057d73f9ae4fbe9c4160a4e23
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2019-08-13 13:53:22 +0630

    Initial Commit

---
+++ CompanyCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyCoreData
+//
+//  Created by Myo Thura Zaw on 8/13/19.
+//  Copyright © 2019 Myo Thura Zaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
